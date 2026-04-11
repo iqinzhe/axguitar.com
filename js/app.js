e79e66af349a09496619a74c910ac90315f7545a
iqinzhe <iqinzhe@gmail.com>
2026-04-11 20:55:10 +0700

    Delete js/app.js

--- js/app.js
+++
@@ -1,8 +0,0 @@
-function renderUIByRole() {
-    const role = DB.currentUser.role;
-
-    if (role !== "SUPER_ADMIN") {
-        const el = document.getElementById("menuUsers");
-        if (el) el.style.display = "none";
-    }
-}
