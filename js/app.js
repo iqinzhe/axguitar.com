721f5cb408325da84fddafe293104e445b4b9eb7
iqinzhe <iqinzhe@gmail.com>
2026-04-11 21:20:20 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -19,13 +19,43 @@ const APP = {
         this.render();
     },
 
-    viewLoans() {
-        this.page = "loans";
+    openOrders() {
+        this.page = "orders";
         this.render();
     },
 
-    pay(id) {
+    createOrder() {
+
+        const name = prompt("Name");
+        const phone = prompt("Phone");
+        const amount = parseFloat(prompt("Amount"));
+        const rate = parseFloat(prompt("Rate"));
+        const collateral = prompt("Collateral");
+
+        DB.loans.push({
+            id: Date.now(),
+            name,
+            phone,
+            principal: amount,
+            remaining: amount,
+            rate,
+            collateral,
+            start: Utils.today(),
+            lastInterest: Utils.today(),
+            staff: AUTH.user.username,
+            history: []
+        });
+
+        this.render();
+    },
+
+    payInterest(id) {
         PaymentService.payInterest(id);
+        this.render();
+    },
+
+    payPrincipal(id) {
+        PaymentService.payPrincipal(id);
         this.render();
     },
 
@@ -38,10 +68,12 @@ const APP = {
             return;
         }
 
-        if (this.page === "dashboard")
+        if (this.page === "dashboard") {
             app.innerHTML = UI.dashboard();
+        }
 
-        if (this.page === "loans")
-            app.innerHTML = UI.loans();
+        if (this.page === "orders") {
+            app.innerHTML = UI.orders();
+        }
     }
 };
