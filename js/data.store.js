bdb177d90f33dc6d3c57785a1fac20fbd56fa7a1
iqinzhe <iqinzhe@gmail.com>
2026-04-11 22:11:36 +0700

    Update data.store.js

--- js/data.store.js
+++ js/data.store.js
@@ -1,22 +1,32 @@
 const DB = {
+
     users: [
-        { username: "admin", password: "123456", role: "admin" },
-        { username: "staff", password: "123456", role: "staff" }
+        {
+            username: "admin",
+            password: "123456",
+            role: "admin"
+        },
+        {
+            username: "staff",
+            password: "123456",
+            role: "staff"
+        }
     ],
 
-    loans: [
-        {
-            id: 1,
-            name: "Budi",
-            phone: "0812",
-            principal: 5000000,
-            remaining: 5000000,
-            rate: 10,
-            collateral: "Gold",
-            start: "2025-01-01",
-            lastInterest: "2025-01-01",
-            staff: "staff",
-            history: []
+    roles: {
+
+        admin: {
+            pages: ["dashboard", "orders", "customers"],
+            actions: ["create", "edit", "delete", "view"]
+        },
+
+        staff: {
+            pages: ["dashboard", "orders"],
+            actions: ["view", "create"]
         }
-    ]
+    },
+
+    loans: []
 };
+
+window.DB = DB;
