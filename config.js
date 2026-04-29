36793e902519ccf189a23807fa06d6f1ca6523c2
iqinzhe <iqinzhe@gmail.com>
2026-04-29 13:37:30 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js - 这个文件不会上传到 GitHub
+window.APP_CONFIG = {
+    SUPABASE: {
+        URL: 'https://hiupsvsbcdsgoyiieqiv.supabase.co',
+        ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpdXBzdnNiY2RzZ295aWllcWl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5ODA3NjYsImV4cCI6MjA5MTU1Njc2Nn0.qL7Qw0I7Ogws_kMoOAae_fCzkhVm-c7NhLPu8rxaJpU'
+    }
+};
