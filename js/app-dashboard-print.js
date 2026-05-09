9df3158f3a83d639a4fe0ec25b23f7f4843db40c
iqinzhe <iqinzhe@gmail.com>
2026-05-10 06:16:48 +0700

    Update app-dashboard-print.js

--- js/app-dashboard-print.js
+++ js/app-dashboard-print.js
@@ -419,7 +419,6 @@
                             <div class="label">${Utils.escapeHtml(item.label)}</div>
                             <div class="value">${Utils.escapeHtml(item.value)}</div>
                         </div>
-                    </div>
                     `).join('')}
                 </div>
             `;
